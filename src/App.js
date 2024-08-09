bc8997927f01a312e01f294799fe5a356110d91d
Owais <owais.it@ramaera.in>
2024-08-09 16:52:45 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className="container"></div>
+        <div className="box_2">
+          <img
+            src="/images/BaliText.png"
+            alt="Bali Text"
+            width="100%"
+            height="100%"
+          />
+        </div>
+        <div className="box_1">
+          <img
+            src="/images/InternationalTourText.png"
+            alt="International Tour Text"
+            width="100%"
+            height="100%"
+          />
+        </div>
+        <div className="box_3">
+          <img
+            src="/images/BaliPhoto.png"
+            alt="Bali Photo"
+            width="100%"
+            height="100%"
+          />
+        </div>
+        <div className="box_4">
+          <img src="/images/Logo.png" alt="Logo" width="100%" height="100%" />
+        </div>
+      </div>
     </div>
   );
 }
